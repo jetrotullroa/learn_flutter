10c7561aa641509b3f6772468ea79efa90fd64a9
Jetro Roa <jetrotullroa@gmail.com>
2021-10-12 22:54:54 +0800

    Locations and Location Detail

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/models/location.dart';
 import 'package:travel_app/screens/location_detail/image_banner.dart';
 import 'package:travel_app/screens/location_detail/text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        ImageBanner("assets/images/kiyomizu-dera.jpg"),
-        TextSection("Title One", "Section 1"),
-        TextSection("Title Two", "Section 2"),
-        TextSection("Title Three", "Section 3"),
-      ],
-    );
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [ImageBanner(location.imagePath), ...textSections(location)]);
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((locationFact) =>
+            TextSection(locationFact.title, locationFact.text))
+        .toList();
   }
 }
